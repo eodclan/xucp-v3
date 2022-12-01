34d6fd8a1d64536ff8f877df52a1904e56a1d32c
Str1k3r <eodstr1k3r@t-online.de>
2022-12-01 12:00:35 +0100

    3.0alpha-5
    @everyone xUCP Pro Update alpha/3.0alpha-5 released to alpha branch
    Updates between alpha/3.0alpha-4 to alpha/3.0alpha-5:
    - Dashboard: Rewrite to PDO (changes)
    
    01.12.2022

--- usercp/dashboard/index.php
+++ usercp/dashboard/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // ************************************************************************************//
 // * xUCP Free
 // ************************************************************************************//
@@ -19,13 +19,13 @@ site_header(DASHBOARD);
 site_navi_logged();
 site_content_logged();
 
-$select_stmt = $db->prepare("SELECT * FROM accounts ORDER by id DESC LIMIT 1");
+$select_stmt = $db->prepare("SELECT * FROM accounts WHERE `id` = ".$_SESSION['username']['secure_first']);
 $select_stmt->execute();
-$row=$select_stmt->fetch(PDO::FETCH_ASSOC);
+$wl_status=$select_stmt->fetch(PDO::FETCH_ASSOC);
 
 if($select_stmt->rowCount() > 0){
 
-    if (htmlentities($row['whitelisted'], ENT_QUOTES, 'UTF-8') == 1) {
+    if (htmlentities($wl_status['whitelisted'], ENT_QUOTES, 'UTF-8') == 1) {
         echo "	
                         <div class='row'>
                             <div class='col-12'>
@@ -61,7 +61,12 @@ if($select_stmt->rowCount() > 0){
                         </div>";
     }
     if(intval($_SESSION['username']['secure_staff']) >= UC_CLASS_SUPPORTER) {
-        echo "
+        $select_stmt = $db->prepare("SELECT * FROM accounts ORDER by id DESC LIMIT 1");
+        $select_stmt->execute();
+        $max_users_status=$select_stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($select_stmt->rowCount() > 0){
+            echo "
                         <div class='row'>
                             <div class='col-xl-6 col-md-6'>
                                 <!-- card -->
@@ -72,16 +77,18 @@ if($select_stmt->rowCount() > 0){
                                             <div class='col-6'>
                                                 <span class='text-muted mb-3 lh-1 d-block text-truncate'>".DASHBOARDUSERS."</span>
                                                 <h4 class='mb-3'>
-                                                    <span class='counter-value' data-target='".htmlentities($row['id'], ENT_QUOTES, 'UTF-8')."'>".htmlentities($dashboard['id'], ENT_QUOTES, 'UTF-8')."</span> ".DASHBOARDUSERS."
+                                                    <span class='counter-value' data-target='".htmlentities($max_users_status['id'], ENT_QUOTES, 'UTF-8')."'>".htmlentities($max_users_status['id'], ENT_QUOTES, 'UTF-8')."</span> ".DASHBOARDUSERS."
                                                 </h4>
                                             </div>
                                         </div>
 										<div class='progress' style='height: 3px'>
-											<div class='progress-bar bg-dash-color-1' role='progressbar' style='width: ".htmlentities($row['id'], ENT_QUOTES, 'UTF-8')."%' aria-valuenow='".htmlentities($row['id'], ENT_QUOTES, 'UTF-8')."' aria-valuemin='0' aria-valuemax='10000'></div>
+											<div class='progress-bar bg-dash-color-1' role='progressbar' style='width: ".htmlentities($max_users_status['id'], ENT_QUOTES, 'UTF-8')."%' aria-valuenow='".htmlentities($max_users_status['id'], ENT_QUOTES, 'UTF-8')."' aria-valuemin='0' aria-valuemax='10000'></div>
 										</div>
                                     </div><!-- end card body -->
                                 </div><!-- end card -->
                             </div><!-- end col -->";
+        }
+
         $select_stmt = $db->prepare("SELECT id FROM xucp_support ORDER by id DESC LIMIT 1");
         $select_stmt->execute();
         $row=$select_stmt->fetch(PDO::FETCH_ASSOC);
@@ -97,7 +104,7 @@ if($select_stmt->rowCount() > 0){
                                             <div class='col-6'>
                                                 <span class='text-muted mb-3 lh-1 d-block text-truncate'>".DASHBOARDSUPPORT."</span>
                                                 <h4 class='mb-3'>
-                                                    <span class='counter-value' data-target='".htmlentities($row['id'], ENT_QUOTES, 'UTF-8')."'>".htmlentities($dashboard['id'], ENT_QUOTES, 'UTF-8')."</span> ".DASHBOARDSUPPORT."
+                                                    <span class='counter-value' data-target='".htmlentities($row['id'], ENT_QUOTES, 'UTF-8')."'>".htmlentities($row['id'], ENT_QUOTES, 'UTF-8')."</span> ".DASHBOARDSUPPORT."
                                                 </h4>
                                             </div>
                                         </div>
